637cf63e7518ed4bb04a4c67aebfaffdbccfed82
Jean Rene <jeanrsh.pro@gmail.com>
2026-02-26 21:15:39 +0100

    Ajout des images et de la structure final puis modifications de certains élément

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -2,13 +2,6 @@ const nav = document.querySelector("nav");
 const openBtn = document.querySelector(".header-btn");
 const closeBtn = document.querySelector(".header-close");
 
-const btnAside = document.querySelector(".btn-nav-second");
-const navAside = document.querySelector(".nav-aside");
-
-btnAside.addEventListener("click", () => {
-  navAside.classList.toggle("open");
-});
-
 openBtn.addEventListener("click", () => {
   nav.classList.add("open");
 });
@@ -16,3 +9,11 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   nav.classList.remove("open");
 });
+
+const btnAside = document.querySelector(".btn-nav-second");
+const navAside = document.querySelector(".nav-aside");
+
+btnAside.addEventListener("click", () => {
+  navAside.classList.toggle("open_side");
+});
+
